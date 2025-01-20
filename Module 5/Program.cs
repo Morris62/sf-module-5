aba1054bd30c204d824c8f48da6a7ac038261d05
a.chaturov <chaturovai@gmail.com>
2025-01-20 18:38:39 +0300

    unit 5.5.3

--- Module 5/Program.cs
+++ Module 5/Program.cs
@@ -4,45 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        var array = GetArrayFromConsole(10);
-        ShowArray(array, true);
-    }
-    private static void ShowArray(int[] array, bool IsSort = false)
-    {
-        var temp = array;
-        if (IsSort)
-        {
-            temp = SortArray(array);
-        }
-        
-        foreach (var item in temp) 
-        {
-            Console.WriteLine(item);
-        }
-    }
-    private static int[] SortArray(int[] array)
-    {
-        for (var i = 0; i < array.Length; i++)
-        {
-            for (var j = i + 1; j < array.Length; j++)
-            {
-                if (array[i] > array[j])
-                {
-                    (array[i], array[j]) = (array[j], array[i]);
-                }
-            }
-        }
+        Console.WriteLine("Введите фразу:");
+        var saidworld = Console.ReadLine();
 
-        return array;
+        Console.WriteLine("Введите глубину эха:");
+        var depth = int.TryParse(Console.ReadLine(), out var result) ? result : 0;
+
+        Echo(saidworld, depth);
     }
-    private static int[] GetArrayFromConsole(int size = 5)
+
+    private static void Echo(string saidworld, int depth)
     {
-        var array = new int[size];
-        for (var i = 0; i < array.Length; i++)
+        var modif = saidworld;
+        if (modif.Length > 2)
         {
-            Console.Write($"Введите элемент массива номер {i}: ");
-            array[i] = int.TryParse(Console.ReadLine(), out var result) ? result : 0;
+            modif = modif.Remove(0, 2);
         }
-        return array;
+        Console.WriteLine("..." + modif);
+        if (depth > 1)
+        {
+            Echo(modif, depth - 1);
+        }
     }
 }
